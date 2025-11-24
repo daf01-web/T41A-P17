1fb14eb706188488e71f11eae09943cd37f98198
daf01-web <171513@upslp.edu.mx>
2025-11-24 08:41:21 -0600

    Create 06_insert_data_JSONB.sql

---
+++ 06_insert_data_JSONB.sql
@@ -0,0 +1,8 @@
+
+INSERT INTO productos (data)
+VALUES 
+  ('{"nombre": "Laptop", "precio": 1200.50, "stock": 16, "color": "Rojo"}'),
+  ('{"nombre": "Teclado", "precio": 75.99, "stock": 40, "color": "Negro"}'),
+  ('{"nombre": "Mouse", "precio": 25.00, "stock": 5, "color": "Verde"}'),
+  ('{"nombre": "Monitor", "precio": 299.99, "stock": 58, "color": "Negro"}'),
+  ('{"nombre": "Silla", "precio": 150.75, "stock": 2, "color": "Rojo"}');
