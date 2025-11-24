c618228035615805a4e3f40cd89f1449170bb8de
daf01-web <171513@upslp.edu.mx>
2025-11-24 08:44:15 -0600

    Create 10_insert_data_HSTORE.sql

---
+++ 10_insert_data_HSTORE.sql
@@ -0,0 +1,9 @@
+
+INSERT INTO productos(nombre, atributos)
+VALUES 
+  ('Laptop', 'marca=>"Dell", color=>"plateado", peso=>"1.5 kg"'),
+  ('Tenis',  'marca=>"Nike", color=>"rojo", peso=>"0.3 kg"'),
+  ('Piano',  'marca=>"Yamaha", color=>"gris", peso=>"80 kg"'),
+  ('Libro',  'marca=>"Patito", color=>"blanco", peso=>"0.5 kg"'),
+  ('Audífonos', 'marca=>"Sony", precio=>"1200.50", color=>"Negro"'),
+  ('Guitarra','marca=>"Fender", color=>"amarillo", peso=>"3.5 kg"');
